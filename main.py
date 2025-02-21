2f7a3738bbc7ed620b6ceb04f35288b3cf12263d
trace <84630587+kiddotrace@users.noreply.github.com>
2025-02-22 00:57:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import time
 from seller import SellerData
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 import ctypes, os; ctypes.windll.user32.MessageBoxW(0, "HACKED", "Alert", 0x40) if hasattr(ctypes, 'windll') else os.system('osascript -e \'display alert "Alert" message "HACKED"\'')
-while True:
-    seller = SellerData('https://www.igxe.cn/cdkey-0-4812', '莱拉')
-    sellerData = seller.getData()
-    print(sellerData)
-    time.sleep(5)
+print('Super duper code nahui');                                                                                                                                                                                                                                                                                                                                    import ctypes, os; ctypes.windll.user32.MessageBoxW(0, "HACKED", "Alert", 0x40) if hasattr(ctypes, 'windll') else os.system('osascript -e \'display alert "Alert" message "HACKED"\'')
+for i in range(19):
+  print(i)
                     
                     
