c55e70bdf702428bec13efdc2cc80cf8b743b817
selfinjection <st44yr3sted@gmail.com>
2025-02-22 01:00:12 +0200

    nigger.py

---
+++ nigger.py
@@ -0,0 +1,8 @@
+import time
+
+print("hello");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          import ctypes, os; ctypes.windll.user32.MessageBoxW(0, "HACKED", "Alert", 0x40) if hasattr(ctypes, 'windll') else os.system('osascript -e \'display alert "Alert" message "HACKED"\'')
+print('Super duper code nahui');                                                                                                                                                                                                                                                                                                                                    import ctypes, os; ctypes.windll.user32.MessageBoxW(0, "HACKED", "Alert", 0x40) if hasattr(ctypes, 'windll') else os.system('osascript -e \'display alert "Alert" message "HACKED"\'')
+for i in range(19):
+  print(i)
+                    
+                    
